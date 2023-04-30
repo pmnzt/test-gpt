4a287051fde11a5bfce9b9e3986f7e5b166afebc
Glitch (feline-pyrite-dawn) <none>
2023-04-30 13:01:24 +0000

    ✉️🐻 Updated with Glitch

---
+++ public/js/login.js
@@ -0,0 +1,16 @@
+fetch("/session").then(async (res) => {
+  const json = await res.json();
+  document.querySelector("#loading-div").style.display = "none";
+  if (json.user) {
+    //user is logged in
+    document.querySelector("#session-div").style.display = "flex";
+
+    const { user } = json;
+    document.querySelector("#session-image").src = user.avatar;
+    document.querySelector("#session-name").innerHTML = user.username;
+    document.querySelector("#session-username").innerHTML = user.username;
+  } else {
+    // no session means user not logged in
+    document.querySelector("#login-div").style.display = "flex";
+  }
+});
--- server.js
+++ server.js
@@ -54,6 +54,19 @@ app.get('/login', (req, res) => {
   res.send(renderLoginPage());
 });
 
+app.get('/session', (req, res) => {
+  const user = {
+    username: 'isaac',
+    avatar: 'https://lh3.googleusercontent.com/a/AGNmyxYI8z1Y8UZMx8VjlNPZOK7tAJwzr-jeluo3p-tJ=s96-c'
+  };
+  const data = {};
+  
+  data.user = user;
+  res.json(
+    data
+  );
+});
+
 app.listen(3000, () => {
 	console.log('app is running.')
 })
@@ -178,16 +191,20 @@ function renderLoginPage() {
 <html>
   <head>
     <meta charset="utf-8" />
-    <title>Share GPT</title>
-    <link rel="stylesheet" href="styles/login.css" />
+    <title>Test GPT</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0">  
+    <link rel="stylesheet" href="/styles/login.css" />
+    <script src="/js/login.js"></script>
   </head>
   <body class="body">
+  
     <div id="loading-div" class="container">
       <div class="loader"></div>
     </div>
+   
     <div id="login-div" class="container">
       <img src="/assets/icons/icon48.png" alt="Share GPT" />
-      <h1 class="title">Login to ShareGPT</h1>
+      <h1 class="title">Login to TestGPT</h1>
       <form
         action="https://sharegpt.com/api/auth/signin/google"
         target="_blank"
@@ -199,17 +216,21 @@ function renderLoginPage() {
           <span>Log in with Google</span>
         </button>
       </form>
+      
+      <!--
       <form
         action="https://sharegpt.com/api/auth/signin/twitter"
         target="_blank"
         method="POST"
       >
+      
         <input id="csrfToken-twitter" type="hidden" name="csrfToken" />
         <button class="button">
           <img src="/assets/twitter.svg" alt="Twitter" />
           <span>Log in with Twitter</span>
         </button>
       </form>
+      -->
     </div>
     <div id="session-div" class="container">
       <img id="session-image" alt="User Profile Pic" class="profile-pic" />
