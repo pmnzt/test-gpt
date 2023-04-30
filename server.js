835accfd50f8f6f51de2b644062f6d18953ac2b8
Glitch (feline-pyrite-dawn) <none>
2023-04-30 16:27:39 +0000

    🌎📳 Updated with Glitch

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const passport = require('passport');
 const dotenv = require('dotenv');
+const axios = require('axios');
 const cookieParser = require("cookie-parser");
 
 const { promises: fsp } = require('fs');
@@ -47,7 +48,7 @@ app.get('/api/', authCheck, (req, res) => {
   res.sendStatus(200);
 })
 
-app.get('/login/google/', passport.authenticate('google', { scope: ['profile'], accessType: 'offline' }));
+app.get('/login/google/', passport.authenticate('google', { scope: ['profile', 'email'], accessType: 'offline' }));
 app.get('/login/google/redirct', passport.authenticate('google', { session:false }), async (req, res) => {
   const userinfo = await authenticateUser(req.user);
   res.cookie('user', JSON.stringify(userinfo), {maxAge: 1704085200 });
@@ -109,9 +110,13 @@ async function authCheck(req, res, next) {
      
 }
 
-async function getUserInfo() {
+async function getUserInfo(token) {
+  const res = await axios.get(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${token}`);
+  let id = 0;
+  const user = res.data;
+  id = user.sub;
   return {
-    id: 0
+    id
   } 
 } 
 
